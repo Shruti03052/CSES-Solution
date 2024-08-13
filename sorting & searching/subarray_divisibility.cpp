8ebf1b1ad21758802883c3faa4cacb9660f19b26
Shruti03052 <120304582+Shruti03052@users.noreply.github.com>
2024-08-14 01:01:03 +0530

    Update subarray_divisibility.cpp

--- sorting & searching/subarray_divisibility.cpp
+++ sorting & searching/subarray_divisibility.cpp
@@ -1,20 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 long long int divisible_subarray(long long int n,vector<long long int> arr){
-    long long int i=0;
+    
     long long int j=0;
     long long int sum=0;
     long long int cnt=0;
+    map<int,int> m;
+    m[0]=1;
     while(j<n){
-        sum+=arr[j];
-        while(i<j && sum>n && sum%n!=0){
-            sum-=arr[i];
-            i++;
-        }
-        if(sum%n==0){
-            cnt++;
-        }
-        j++;
+       sum+=arr[j];
+       int rem=sum%n;
+       if(rem<0){
+        rem=rem+n;
+       }
+       if(m.find(rem)==m.end()){
+        m[rem]=1;
+
+
+       }
+       else{
+        cnt+=m[rem];
+        m[rem]+=1;
+       }
+       j++;
 
 
     }
